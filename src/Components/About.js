308041c19e9c528e4811570af1548d04044f488b
JATHIN KUMAR REDDY YATTAPU <y.jathinkumar@gmail.com>
2025-05-15 18:41:55 +0530

    img change

--- src/Components/About.js
+++ src/Components/About.js
@@ -5,7 +5,7 @@ const About = () => {
       <section id="about" className="about-section">
         <h2 className="section-title">About Me</h2>
         <div className="about-content">
-          <img src="https://media.licdn.com/dms/image/v2/D5603AQEt-W-ilwYryQ/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1729592534742?e=1746662400&v=beta&t=0YjIpqAor5-G9j205J1MO6ECtkjydnIn9LnjjFQwyxc" alt="Jathin" className="profile-pic" />
+          <img src="https://lh3.googleusercontent.com/a/ACg8ocKid7YWKavFZd6uoyY3IOoXXX3Si3EQX_lGot7_eNS_XGHz0VV4vg=s192-c-rg-br100" alt="Jathin" className="profile-pic" />
           <p>
             Hi, I'm Jathin, a full-stack developer with expertise in .NET and MERN stack.
             I love building scalable web applications and solving complex problems.
